3e6a2acbb8d0ef72c7b890e27d0f5e88fb7d7a6e
Christian Montoya <chrimonlo@gmail.com>
2024-02-06 21:59:36 -0500

    feat(app):Integration of the components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Description } from './components/home/Description/Description';
+import { Hero } from './components/home/Hero/Hero';
+import { MainProducts } from './components/home/MainProducts/MainProducts';
 export default function Home() {
 	return (
 		<main>
-			<h1>Next js - Store</h1>
+			<Hero />
+			<Description />
+			<MainProducts />
 		</main>
 	);
 }
