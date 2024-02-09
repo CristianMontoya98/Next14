98a8692a586062f144025e296c964c4e470c6bb1
Christian Montoya <chrimonlo@gmail.com>
2024-02-09 18:43:15 -0500

    styles(hero):adding styles in hero texts

--- src/app/components/home/Hero/Hero.tsx
+++ src/app/components/home/Hero/Hero.tsx
@@ -2,7 +2,8 @@ import styles from './hero.module.css';
 export const Hero = () => {
 	return (
 		<section className={styles.hero}>
-			<h1>Hero</h1>
+			<h1>Future world!</h1>
+			<h2>Empowering Your Tomorrow, Today!</h2>
 		</section>
 	);
 };
