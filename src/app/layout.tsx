b0fdd9cbd35837a72941511778e9dd540b4a4cf7
Christian Montoya <chrimonlo@gmail.com>
2024-02-22 22:16:13 -0500

    feat(layout):Font optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Roboto } from 'next/font/google';
 import { Header } from './components/shared/Header/Header';
 import { Footer } from './components/shared/Footer/Footer';
 import 'app/sass/globals.scss';
-const inter = Inter({ subsets: ['latin'] });
+const roboto = Roboto({
+	weight: ['100', '300', '500', '700'],
+	subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
 	title: 'Future world',
@@ -17,7 +20,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>
+			<body className={roboto.className}>
 				<Header />
 				{children}
 				<Footer />
