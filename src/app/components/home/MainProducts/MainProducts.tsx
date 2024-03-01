bacdd09ff07a69085f5c8a26a9186a643289e2fb
Christian Montoya <chrimonlo@gmail.com>
2024-02-29 21:54:33 -0500

    feat(mainProduct):Add rendering of the products

--- src/app/components/home/MainProducts/MainProducts.tsx
+++ src/app/components/home/MainProducts/MainProducts.tsx
@@ -1,25 +1,45 @@
+import Image from 'next/image';
+import styles from './mainProducts.module.scss';
+
 const getProducts = async () => {
-	const response = await fetch(
-		`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
-		{
-			headers: new Headers({
-				'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || '',
-			}),
-		}
-	);
-	const data = await response.json();
-	return data;
+	try {
+		const response = await fetch(
+			`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
+			{
+				headers: new Headers({
+					'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || '',
+				}),
+			}
+		);
+		const { products } = await response.json();
+		return products;
+	} catch (error) {
+		console.log(error);
+	}
 };
-/* Los componentes asincronos son una 
-caracteristica del server side rendering de nextjs
-que permite ejecutar peticiones si usar un useEffect 
-como se haria con react */
+
 export const MainProducts = async () => {
 	const products = await getProducts();
 
 	return (
-		<section>
-			<h1>Main Products</h1>
+		<section className={styles.MainProducts}>
+			<h3>✨ New products released!</h3>
+			<div className={styles.MainProducts__grid}>
+				{products?.map((product: any) => {
+					const imageSrc = product.images[0].src;
+					return (
+						<article key={product.id}>
+							<p>{product.title}</p>
+							<Image
+								src={imageSrc}
+								fill
+								alt={product.title}
+								loading='eager'
+							/>
+						</article>
+					);
+				})}
+			</div>
 		</section>
 	);
 };
