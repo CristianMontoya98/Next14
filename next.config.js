006f1701a97c8b361a6b26a93780d1a246691473
Christian Montoya <chrimonlo@gmail.com>
2024-02-29 21:54:05 -0500

    feat(nextConfig):add config to images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 
+const { hostname } = require('os');
 const path = require('path');
 
 const nextConfig = {
 	sassOptions: {
 		includePaths: [path.join(__dirname, 'src/sass')],
+	},
+	images: {
+		remotePatterns: [
+			{
+				hostname: 'cdn.shopify.com',
+				protocol: 'https',
+			},
+		],
 	},
 };
 module.exports = nextConfig;
