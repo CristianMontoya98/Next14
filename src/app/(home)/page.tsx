c15900ca8e6beacdc9cdc3e869cb2f02c387e479
Christian Montoya <chrimonlo@gmail.com>
2024-03-14 21:50:42 -0500

    feat(page):Add page component to the home folder

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import { Description } from '../components/home/Description/Description';
+import { MainProducts } from '../components/home/MainProducts/MainProducts';
+export default function Home() {
+	return (
+		<main>
+			<MainProducts />
+		</main>
+	);
+}
