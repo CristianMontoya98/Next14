c66005acee163d01ecf946aa40b7832440a15cdd
Christian Montoya <chrimonlo@gmail.com>
2024-04-02 22:04:44 -0500

    fix(store): Change the get collections fetching

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,5 +1,4 @@
 import { ProductsWrapper } from 'app/app/components/store/ProductsWrapper/ProductsWrapper';
-import { getCollections } from 'app/app/services/shopify/collections';
 import { getProducts } from 'app/app/services/shopify/products';
 
 interface CategoryProps {
@@ -11,8 +10,7 @@ interface CategoryProps {
 
 export default async function Category(props: CategoryProps) {
 	const products = await getProducts();
-	const collections = await getCollections();
-	console.log(collections);
+
 	const { categories } = props.params;
 	return <ProductsWrapper products={products} />;
 }
