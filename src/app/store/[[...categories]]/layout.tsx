689196ab48fb81495be8bb959cda89c7976c8630
Christian Montoya <chrimonlo@gmail.com>
2024-04-03 22:03:42 -0500

    feat(storeLayout): Add the styles to the layout

--- src/app/store/[[...categories]]/layout.tsx
+++ src/app/store/[[...categories]]/layout.tsx
@@ -1,6 +1,6 @@
 import { getCollections } from 'app/app/services/shopify/collections';
 import Link from 'next/link';
-
+import styles from '../layout.module.scss';
 export default async function Layout({
 	children,
 }: {
@@ -8,16 +8,20 @@ export default async function Layout({
 }) {
 	const collections = await getCollections();
 	return (
-		<main>
+		<main className={styles.StoreLayout}>
+			<h1>Explore</h1>
 			<nav>
-				{collections.map((collection: any) => (
-					<Link
-						key={collection.id}
-						href={'/store/' + collection.handle}
-					>
-						{collection.title}
-					</Link>
-				))}
+				<ul className={styles.StoreLayout__list}>
+					{collections.map((collection: any) => (
+						<Link
+							key={collection.id}
+							href={'/store/' + collection.handle}
+							className={styles.StoreLayout__chip}
+						>
+							{collection.title}
+						</Link>
+					))}
+				</ul>
 			</nav>
 			{children}
 		</main>
