74e7051b758c63276a2a2c2456834da1466e5891
Christian Montoya <chrimonlo@gmail.com>
2024-04-10 23:40:39 -0500

    feat(product):Add the product props interface

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,3 +1,8 @@
-export default async function ProductPage() {
+interface ProductPageProps {
+	searchParams:{
+		id:string;
+	}
+}
+export default async function ProductPage(props:ProductPageProps) {
 	return <h1>Product Page</h1>;
 }
