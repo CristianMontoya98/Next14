74af794632bd3553bfd4a529e8577e8a9eb0f4cc
Christian Montoya <chrimonlo@gmail.com>
2024-05-14 22:45:05 -0500

    feat(cache): Create the cache route

---
+++ src/app/api/cache/route.ts
@@ -0,0 +1,7 @@
+export async function POST(request:Request){
+    const body = await request.json();
+    const {tag, token} = body;
+    if(!tag||!token){
+        return Response.json({error:"Missing tag or token"},{status:400})
+    }
+}
