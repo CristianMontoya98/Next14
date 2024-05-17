4c0143c0ea1370f7ca22dd697c7ba9b6e0c6afb4
Christian Montoya <chrimonlo@gmail.com>
2024-05-16 22:05:09 -0500

    fix(cacheRoute): add title to the error message

--- src/app/api/cache/route.ts
+++ src/app/api/cache/route.ts
@@ -2,6 +2,6 @@ export async function POST(request:Request){
     const body = await request.json();
     const {tag, token} = body;
     if(!tag||!token){
-        return Response.json({error:"Missing tag or token"},{status:400})
+        return Response.json({error:"ERROR: Missing tag or token"},{status:400})
     }
 }
