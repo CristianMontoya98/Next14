6596a251218caf96e7157620e606725d2761a63f
Christian Montoya <chrimonlo@gmail.com>
2025-05-02 16:35:00 -0500

    feat(productPage): Add redirect when there is not id in params

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,17 +1,18 @@
-
-import { ProductView } from "app/app/components/product/ProductView/ProductView"
-import { getProducts } from "app/app/services/shopify/products"
-
+import { ProductView } from 'app/app/components/product/ProductView/ProductView';
+import { getProducts } from 'app/app/services/shopify/products';
+import { redirect } from 'next/navigation';
 interface ProductPageProps {
-  searchParams: {
-    id: string
-  }
+    searchParams: {
+        id: string;
+    };
 }
 
 export default async function ProductPage({ searchParams }: ProductPageProps) {
-  const id = searchParams.id;
-  const products = await getProducts(id);
-  const product = products[0];
-
-  return <ProductView product={product} />
+    const id = searchParams.id;
+    const products = await getProducts(id);
+    const product = products[0];
+    if (!id) {
+        redirect('/store');
+    }
+    return <ProductView product={product} />;
 }
