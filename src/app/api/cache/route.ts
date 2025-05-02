a5c537addc9e6804e6ef4396ac4fe0f8d0a94a5a
Christian Montoya <chrimonlo@gmail.com>
2025-05-02 16:26:12 -0500

    feat(route): Re validate path added and new route for path created

--- src/app/api/cache/route.ts
+++
@@ -1,7 +0,0 @@
-export async function POST(request:Request){
-    const body = await request.json();
-    const {tag, token} = body;
-    if(!tag||!token){
-        return Response.json({error:"ERROR: Missing tag or token"},{status:400})
-    }
-}
